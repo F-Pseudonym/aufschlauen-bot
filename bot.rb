c7a85269619fa25bf3e03e5b48dab5daa1823680
Florian Scheuer <coding@florian-scheuer.de>
2017-12-03 16:18:08 +0100

    fixed path issue

--- bot.rb
+++ bot.rb
@@ -2,12 +2,12 @@
 
 require 'twitter'
 require 'parseconfig'
-require './bullshit.rb'
+require "#{File.dirname(__FILE__)}/bullshit.rb"
 
 
 sentence = "#{@part1[rand(@part1.size)]} #{@part2[rand(@part2.size)]} #{@part3[rand(@part3.size)]} #{@part4[rand(@part4.size)]} #{@part5[rand(@part5.size)]}#{@part6[rand(@part6.size)]}#{@part7[rand(@part7.size)]}"
 
-stored_config = ParseConfig.new('./twitter.config')
+stored_config = ParseConfig.new("#{File.dirname(__FILE__)}/twitter.config")
 
 client = Twitter::REST::Client.new do |config|
   config.consumer_key        = stored_config["consumer_key"]
